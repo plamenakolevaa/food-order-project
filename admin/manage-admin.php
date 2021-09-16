9de485e263fff10b94ce5d04a15c85d13f8fae85
Plamena Koleva <plamena.koleva11@gmail.com>
2021-09-16 19:35:30 +0300

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,51 @@
+<?php include('partials/menu.php') ?>
+
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+                <br />
+                <a href="#" class="btn-primary">Add Admin</a>
+                <br /><br />
+
+            <table class = "tbl-full">
+             <tr>
+                    <th>Number</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>        
+            </tr>
+                 <tr>
+                <td>1.</td>
+                <td>Plamena Koleva</td><br />
+                <td>plamenakoleva</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a><br /><br />
+                    
+                </td>
+                
+             </tr>
+             
+            <tr>
+                <td>2.</td>
+                <td>Plamena Koleva</td>
+                <td>plamenakoleva</td>
+                <td>
+                <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a><br /><br />
+                </td>
+            </tr>
+            <tr>
+                <td>3.</td>
+                <td>Plamena Koleva</td>
+                <td>plamenakoleva</td>
+                <td>
+                <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a><br /><br />
+                </td>
+            </tr>
+            </table>
+            </div>
+        </div>
+        <?php include('partials/footer.php'); ?>
+</html>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer starts section -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">All rights reserved. Developed by - Plamena Koleva</p>
+            </div>
+        </div>
+        <!-- Footer ends -->
+    </body>
+</html>
